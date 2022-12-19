537d8a94f74f65f97ce1bd930eb86bc00f6721b8
jqbmaranan <james.maranan.512@gmail.com>
2022-12-19 11:43:58 +0800

    Copied sir repo manually

--- pychat/__main__.py
+++ pychat/__main__.py
@@ -4,18 +4,16 @@ import os
 OPENAI_API_KEY='OPENAI_API_KEY'
 
 def main():
-    openai.api_key = os.getenv(OPENAI_API_KEY)
+    openai_api_key = os.getenv(OPENAI_API_KEY)
 
-    if openai.api_key == None:
-        print("OPENAI_API_KEY")
+    if openai_api_key == None:
+        print("OPENAI_API_KEY required")
         exit(-1)
 
     print("PyChat v0.1")
     query = input('Input: ')
 
-    print("Query: {}".format(query))
-    
-    completion = openai.Completion.create(engine="text-davinci-003", prompt=query, max_tokens=4000)
+    completion = openai.Completion.create(engine='text-davinci-003', prompt=query, max_tokens=4000)
 
     output = completion.choices[0].text
 
