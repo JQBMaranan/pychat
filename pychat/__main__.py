b1543fb6effefdff48196e1b99693593f00a7556
jqbmaranan <james.maranan.512@gmail.com>
2022-12-19 10:52:46 +0800

    initial commit

---
+++ pychat/__main__.py
@@ -0,0 +1,16 @@
+import openai
+
+def main():
+    print("PyChat v0.1")
+    query = input('Input: ')
+
+    print("Query: {}".format(query))
+    
+    completion = openai.Completion.create(engine="text-davinci-003", prompt=query, max_tokens=4000)
+
+    output = completion.choices[0].text
+
+    print("Output: {}".format(output))
+
+if __name__ == '__main__':
+    main()
